83ec077dac1bec2096e79e41c42ca485fb2c98be
Breather08 <you@example.com>
2020-11-30 19:46:21 +0600

    rotation added

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import createElement from "./utils/createElement.js";
 
 const Scene = () => {
+  const keyState = {};
   const sceneDOM = createElement({
     tagName: "div",
     classNames: "scene",
@@ -9,8 +10,10 @@ const Scene = () => {
   document.body.append(sceneDOM);
   return {
     DOM: sceneDOM,
+    RECT: sceneDOM.getBoundingClientRect(),
+    keyState: keyState,
     position() {
-      let sceneBox = sceneDOM.getBoundingClientRect();
+      let sceneBox = this.RECT;
 
       return {
         top: Math.round(sceneBox.top),
@@ -19,18 +22,30 @@ const Scene = () => {
         height: Math.round(sceneBox.height),
       };
     },
+    startKeyEvents() {
+      sceneDOM.onkeydown = sceneDOM.onkeyup = (event) => {
+        console.log(keyState);
+        if (event.type === "keydown") {
+          keyState[event.keyCode] = true;
+        } else if (event.type === "keyup") {
+          keyState[event.keyCode] = false;
+        }
+      };
+    },
   };
 };
 
-const gameObject = ({
+const GameObject = ({
   className,
   id,
   src,
   element,
   initialCoords = [0, 0],
-  movePoints = [1, 1],
 }) => {
-  let [dx, dy] = movePoints;
+  let rotate = 0,
+    scaleX = 1,
+    scaleY = 1;
+
   let [x, y] = initialCoords;
   if (!element) {
     element = createElement({
@@ -43,18 +58,51 @@ const gameObject = ({
               attrs: [["src", `${src}`], id ? ["id", `${id}`] : []],
               width: "100%",
               height: "100%",
-              r,
             }),
           ]
         : [],
     });
   }
+  element.style.transformOrigin = "center";
+
+  const change = ({
+    tlX = x,
+    tlY = y,
+    rot = rotate,
+    scX = scaleX,
+    scY = scaleY,
+  }) => {
+    element.style.transform = `matrix(
+      ${scX}, 
+      ${rot}, 
+      ${-rot}, 
+      ${scY}, 
+      ${tlX}, 
+      ${tlY})`;
+  };
+
   return {
     DOM: element,
-    update(stepX, stepY) {
-      x += stepX;
-      y += stepY;
-      element.style.transform = `translate(${x}px, ${y}px)`;
+    translate(dx = 0, dy = 0) {
+      x += dx;
+      y += dy;
+      change({ tlX: x, tlY: y });
+    },
+    rotate(value = 0, measure = "deg") {
+      let xRot = Math.sqrt(1 / (Math.tan((value * Math.PI) / 180) + 1));
+      let yRot = Math.sqrt(1 - xRot ** 2);
+      console.log(Math.tan((value * Math.PI) / 180), xRot, yRot);
+      rotate += value;
+      change({
+        scX: yRot * scaleX,
+        scY: yRot * scaleY,
+        rot: xRot,
+      });
+    },
+    scale(x = 1, y = 1) {
+      scaleX += x;
+      scaleY += y;
+      change({ scX: scaleX, scY: scaleY });
     },
     position() {
       let sceneBox = document.querySelector(".scene").getBoundingClientRect();
@@ -67,31 +115,21 @@ const gameObject = ({
         height: Math.round(gameObjBox.height),
       };
     },
-    setPosition(posX, posY) {
-      x = posX;
-      y = posY;
-    },
   };
 };
 
-let dx = 1, dy = 1
+// testing
 const scene = Scene();
-const ball = gameObject({ className: "ball" });
+const ball = GameObject({ className: "ball" });
 scene.DOM.append(ball.DOM);
 
-const test = () => {
-  if (
-    ball.position().top - ball.position().width * 2 ===
-    scene.position().top + 2
-  ) {
-    dy = -dy
-  }
-  ball.update(dx, dy);
-};
+scene.startKeyEvents();
 
+// ball.translate(10, 10);
+ball.rotate(110, "deg");
+// ball.DOM.style.transform = 'rotate(0.125turn)'
 const animate = () => {
-  test()
   requestAnimationFrame(animate);
 };
 
-// animate();
+animate();
